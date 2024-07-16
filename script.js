9862c1f7c036d69a2f5018fc006a6bb8faa46356
Caio Wilker <148403308+caiowilker@users.noreply.github.com>
2024-07-15 22:07:19 -0300

    finalizando

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
     // Script para funcionalidade adicional futura
 });
 
-function confirmPurchase(productName) {
+function confirmPurchase(productName, productLink) {
     document.getElementById('product-name').innerText = productName;
+    document.getElementById('purchase-link').href = productLink;
     document.getElementById('confirmation-popup').classList.remove('hidden');
 }
 
